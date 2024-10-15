e8f3efec9bc132968bc44d69e70c44c7ca8910c0
hmoodaps <ahmadnahal7@gmail.com>
2024-10-15 18:37:08 +0200

    final

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -2,11 +2,13 @@
 import os
 from pathlib import Path
 
+from django.template.context_processors import media
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-3ko7p#b@@gr=k+je2wlx4ju_^dcwz)-(0fj!9=8ffu1pmixr_$'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -62,12 +64,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject3.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,6 +92,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+MEDIA_URLS ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# تحديد إعدادات Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inshala.settings')
 
-app = get_wsgi_application()
-
+# إنشاء تطبيق WSGI
+application = get_wsgi_application()
